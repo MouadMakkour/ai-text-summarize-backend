bd94b5d23573e671e06423eb6fd3f568c51a8751
unknown <mouadmakkour1900@gmail.com>
2026-01-29 17:24:10 +0100

    fix cors issue 2

--- app/main.py
+++ app/main.py
@@ -4,17 +4,16 @@ from app.api.summarize import router as summarize_router
 
 app = FastAPI(title="AI Text Summarizer API")
 
-origins = [
-    "https://ai-text-summarize-frontend-bolf6y7n1-mouad-makkours-projects.vercel.app",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=[
+        "https://ai-text-summarize-frontend-bolf6y7n1-mouad-makkours-projects.vercel.app",
+    ],
+    allow_credentials=False,  # 👈 IMPORTANT
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(summarize_router)
 
